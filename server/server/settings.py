128052d55891abe4f9fcc78a1b52fe73b3eeeee4
Daniel <madrugadjn@gmail.com>
2019-10-12 23:21:46 +0100

    temporary template made

--- server/server/settings.py
+++ server/server/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'zawa.apps.ZawaConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- server/zawa/models.py
+++ server/zawa/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.username
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=70)
+    description = models.TextField()
+    pub_date = models.DateField()
+    beginning = models.DateField()
+    owner = models.ForeignKey(User, on_delete = models.CASCADE)
+    type = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title

487fce3c2072743469c70a5aad99994e3b716dfb
Andre Baptista Neves Ribeiro <andre@localhost.localdomain>
2019-10-12 15:09:17 +0100

    new structure

---
+++ zawa/src/store.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+export const store = Vue.observable({
+  isNavOpen: false
+})
+
+// We call toggleNav anywhere we need it in our app
+export const mutations = {
+  toggleNav () {
+    store.isNavOpen = !store.isNavOpen
+  }
+}
